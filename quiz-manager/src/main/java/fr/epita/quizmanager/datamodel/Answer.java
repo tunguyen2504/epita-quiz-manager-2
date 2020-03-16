2191da479ccd7808263a3bcc561040eadcc86fbc
Nguyễn Anh Tú <tunguyen.hust@gmail.com>
2020-03-16 01:52:08 +0100

    first commit

---
+++ quiz-manager/src/main/java/fr/epita/quizmanager/datamodel/Answer.java
@@ -0,0 +1,35 @@
+package fr.epita.quizmanager.datamodel;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ANSWERS")
+public class Answer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String answer;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	
+}
